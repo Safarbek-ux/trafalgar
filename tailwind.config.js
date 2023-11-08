278ae290707e9e66857d56803baf91011e2c55c5
safarbek-ux <safarbekyunusov@gmail.com>
2023-11-08 10:38:32 +0500

    Tayyor

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,8 @@ module.exports = {
       backgroundImage: {
         'svg_smal': "url('../src/images/svg_bg.svg')",
         'svg_our': "url('../src/images/our_bg.svg')",
+        'svg_right': "url('../src/images/Vectorrigtht.png')",
+        'customers': 'linear-gradient(208deg, #67C3F3 9.05%, #5A98F2 76.74%)',
       },
       boxShadow:{
         'lishadow' :'10px 40px 50px 0px rgba(229, 233, 246, 0.40)',
